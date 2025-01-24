19f0326cb868fb9cae983ef4c5c49e33782318c9
Muhammad Quraisy <iye.fredickson@gmail.com>
2025-01-24 22:24:52 +0700

    add CORS and cookie-parser middleware, and include assignment route in the application

--- backend/src/core/app.ts
+++ backend/src/core/app.ts
@@ -1,12 +1,18 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import userRoute from "../routes/user-route";
 import errorMiddleware from "../middleware/error-middleware";
+import assignmentRoute from "../routes/assignment-route";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.use(userRoute);
+app.use(assignmentRoute);
 app.use(errorMiddleware);
 
 export default app;
